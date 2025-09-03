373067c6b88288cbb3557cd58b379a2486d23bd0
Diego Ramos Inácio <97482905+DiegoRInacio@users.noreply.github.com>
2025-09-03 18:02:44 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 class Pessoa:
-    def __init__(self):
-        self.nome = None
-        self.idade = None
-        self.altura = None
-    
+    def __init__(self, nome=None, idade=None, altura=None):
+        self.nome = nome
+        self.idade = idade
+        self.altura = altura
+
     @classmethod
     def cadastro(cls):
         nome = input("Digite seu nome: ")
         idade = int(input("Digite sua idade: "))
         altura = float(input("Digite sua altura: "))
 
-        cls(nome, idade, altura)
-        
+        return cls(nome, idade, altura)  # Retorna a nova instância
